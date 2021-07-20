487ffbc5bf44b54d5876126fbf7c40c0895e700f
Steve Black <scblack@tacc.utexas.edu>
2021-07-20 15:49:27 -0500

    Update docker_build.sh. release now handled by release-*.sh.

--- release/docker_build.sh
+++ release/docker_build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 # Build and optionally push docker image for Systems service
-# This is the job run in Jenkins as part of job Java-MultiService-Build-Publish-Deploy-<ENV>
-#   and job Tapis jobs->DEV->release-systems-start
+# This is the job run in Jenkins as part of job TapisJava->3_ManualBuildDeploy->systems
 # Environment name must be passed in as first argument
 # Existing docker login is used for push
 # Docker image is created with a unique tag: tapis/<SVC_NAME>-<ENV>-<VER>-<COMMIT>-<YYYYmmddHHMM>
 #   - other tags are created and updated as appropriate
 #
-# REPO env var may be set to push to $REPO/${SVC_NAME}. Default is tapis/${SVC_NAME}
 # Env var TAPIS_DEPLOY_MANUAL may be set to "true" to indicate it is a manual deployment and the
 #   image should also be tagged with $ENV
 
@@ -16,10 +14,7 @@ PrgName=$(basename "$0")
 USAGE="Usage: $PrgName { dev staging prod } [ -push ]"
 
 SVC_NAME="systems"
-
-if [ -z "$REPO" ]; then
-  REPO=tapis
-fi
+REPO="tapis"
 
 BUILD_DIR=../tapis-systemsapi/target
 ENV=$1
@@ -65,9 +60,7 @@ cd $BUILD_DIR || exit
 VER=$(cat classes/tapis.version)
 GIT_BRANCH_LBL=$(awk '{print $1}' classes/git.info)
 GIT_COMMIT_LBL=$(awk '{print $2}' classes/git.info)
-TAG_UNIQ="${REPO}/${SVC_NAME}:${ENV}-${VER}-$(date +%Y%m%d%H%M)-${GIT_COMMIT}"
-TAG_RELEASE_CANDIDATE="${REPO}/${SVC_NAME}:${VER}-rc1"
-# TAG_ENV_VER="${REPO}/${SVC_NAME}:${ENV}-${VER}"
+TAG_UNIQ="${REPO}/${SVC_NAME}:${ENV}-${VER}-$(date +%Y%m%d%H%M)-${GIT_COMMIT_LBL}"
 TAG_ENV="${REPO}/${SVC_NAME}:${ENV}"
 TAG_LATEST="${REPO}/${SVC_NAME}:latest"
 TAG_LOCAL="${REPO}/${SVC_NAME}:dev_local"
@@ -89,11 +82,6 @@ docker build -f Dockerfile \
     -t "${TAG_UNIQ}" .
 
 # Create other tags for remote repo
-# echo "Creating ENV_VER image tag: $TAG_ENV_VER"
-# docker tag "$TAG_UNIQ" "$TAG_ENV_VER"
-echo "Creating RELEASE_CANDIDATE image tag: $TAG_RELEASE_CANDIDATE"
-docker tag "$TAG_UNIQ" "$TAG_RELEASE_CANDIDATE"
-
 echo "Creating image for local testing user tag: $TAG_LOCAL"
 docker tag "$TAG_UNIQ" "$TAG_LOCAL"
 
@@ -106,8 +94,6 @@ if [ "x$2" = "x-push" ]; then
   echo "Pushing images to docker hub."
   # NOTE: Use current login. Jenkins job does login
   docker push "$TAG_UNIQ"
-  docker push "$TAG_RELEASE_CANDIDATE"
-#  docker push "$TAG_ENV_VER"
   if [ "x$TAPIS_DEPLOY_MANUAL" = "xtrue" ]; then
     echo "Creating ENV image tag: $TAG_ENV"
     docker tag "$TAG_UNIQ" "$TAG_ENV"
