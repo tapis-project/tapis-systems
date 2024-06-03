d2522a995e7efb8eeea8796ddc659d3773b85bd3
Steve Black <scblack@tacc.utexas.edu>
2024-06-03 11:17:07 -0500

    Branch filter-by-credentials. WIP.

--- tapis-systemslib/src/main/resources/db/migration/V007__addCredInfoToLoginTable.sql
+++ tapis-systemslib/src/main/resources/db/migration/V007__addCredInfoToLoginTable.sql
@@ -12,36 +12,58 @@
 --);
 --
 
+-- Add new columns:
+--   is_dynamic - indicates if record is for the static or dynamic effectiveUserId case
+--   has_credentials - indicates if system has credentials registered for the current defaultAuthnMethod
+--   has_password - indicates if credentials for PASSWORD have been registered.
+--   has_pki_keys - indicates if credentials for PKI_KEYS have been registered.
+--   has_access_key - indicates if credentials for ACCESS_KEY have been registered.
+--   has_token - indicates if credentials for TOKEN have been registered.
+--   has_cert - indicates if credentials for CERT have been registered.
+--   TODO sync_status - enum indicating current status of synchronization between SK and Systems service.
+--      PENDING - Record requires synchronization
+--      IN_PROGRESS - We are in the process of synchronizing record
+--      FAILED - Synchronization failed.
+--      COMPLETE - Synchronization completed successfully.
+--   TODO sync_fail_message -
+--   TODO sync_fail_count -
+-- Have all columns not null except has_credentials. Use has_credentials as a flag indicating values need to be
+--    filled in from SK queries
+-- Default for is_dynamic is true since if there is an existing record then it is for a login user mapping and that
+--    is only used for dynamic.
+--
+-- Remove the NOT NULL constraint on the login_user column since now we will have records even if there
+--    is no mapping.
+--
+-- Change primary key from (tenant, system_id, tapis_user) to (tenant, system_id, tapis_user, is_dynamic)
+--
 -- TODO - check code to make sure entries in the login_user table are deleted when a system is deleted
 -- TODO - or a credential is deleted.
 --      No, not deleted when system is deleted. But that is probably okay.
---      Creds only deleted for static user case, currently login_user table only for dynamic use case,
---      so thinking was probably that okay to leave creds around for dynamic user case.
+--      Credentials only deleted for static user case, currently login_user table only for dynamic use case,
+--      so thinking was probably that okay to leave credentials around for dynamic user case.
 --      Not okay for static user case because it is a more dangerous service account.
---      Since dynamic user creds left in place it makes sense to leave login_user entries in place.
+--      Since dynamic user credentials left in place it makes sense to leave login_user entries in place.
 -- TODO BUT what about when cred itself is deleted? Yes, login mapping is removed in this case
 -- TODO         (only for dynamic user, since should only be a mapping for dynamic users.)
 -- TODO   NOTE: on delete cascade does not help because systems can only be soft deleted by users.
--- TODO         on delete cascace will only apply if we ever support a hard delete or a db admin is
+-- TODO         on delete cascade will only apply if we ever support a hard delete or a db admin is
 -- TODO         manually cleaning up system records
--- Have all columns not null except has_creds.
---   TODO/TBD - use has_creds as a flag indicating values need to be filled in from SK queries
---           default for is_dynamic is true since if there is an existing record then it is for a
---           login user mapping and that is only used for dynamic.
--- BUT, how to tell if there is a host login user for a dynamic user?
---      TBD: allow login_user to be empty string to indicate there is no mapping?
+ALTER TABLE systems_login_user ALTER COLUMN login_user DROP NOT NULL;
 ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_credentials boolean BOOLEAN;
 ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS is_dynamic boolean BOOLEAN NOT NULL DEFAULT true;
 ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_password boolean BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_pki boolean BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_key boolean BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_pki_keys boolean BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_access_key boolean BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_token boolean BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS has_cert boolean BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS sync_status TEXT NOT NULL DEFAULT 'PENDING';
+ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS sync_fail_count INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE systems_login_user ADD COLUMN IF NOT EXISTS sync_fail_message TEXT;
 
 -- We need to add is_dynamic to the primary key because static and dynamic users are independent even if
 --   the username is identical. Credentials are registered and stored in SK separately.
--- TODO drop existing primary key
+-- Drop existing primary key
 ALTER TABLE systems_login_user DROP CONSTRAINT systems_login_user_pkey;
--- TODO create new primary key
+-- Create new primary key
 ALTER TABLE systems_login_user ADD CONSTRAINT primary key (tenant, system_id, tapis_user, is_dynamic)
-
